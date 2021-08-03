baf1a3bf97d378c4b6223815a65648c11a5a32f8
Mohan Kumar S <mohankumars4723@gmail.com>
2021-08-04 00:31:34 +0530

    Controllers and services concept introduced

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,235 +1,54 @@
 //File used to define controllers for user routes
 
+const { response } = require("express");
 const { validationResult } = require("express-validator");
-const base64 = require("base-64");
-const user = require("../models/userModel");
-const { logger } = require("../logger");
-const { dbSave, dbFind, dbUpdate } = require("../db/dbOperations");
+const {
+  validationError,
+  signupService,
+  signinService,
+  signoutService,
+} = require("../services/userServices");
 
 //Signup controller
-const signUp = (req, res) => {
+const signUp = async (req, res) => {
   //ValidationResult is from middleware used in signup route
   const errors = validationResult(req);
-  //logger.log(errors); For debugging only
-  if (!errors.isEmpty()) {
-    logger.log({
-      level: "error",
-      email: req.body.email,
-      message: "Invalid email address or password format.",
-    });
-    return res.status(400).json({
-      status: "400",
-      message: "Invalid email address or password format.",
-    });
-  }
-  //Checking if email address is already present in DB
+  const email = req.body.email;
+  const password = req.body.password;
   const filter = { email: req.body.email };
-  dbFind(user, filter)
-    .then((document) => {
-      if (document.length !== 0) {
-        logger.log({
-          level: "error",
-          email: req.body.email,
-          message: "Email address already in use.",
-        });
-        return res
-          .status(400)
-          .json({ code: "400", message: "Email address already in use." });
-      } else {
-        //Encoding the password in base64 format
-        const encodedPassword = base64.encode(req.body.password);
-        //Create the document
-        const document = new user({
-          email: req.body.email,
-          password: encodedPassword,
-        });
-        //If no error occurs when saving the document
-        dbSave(document)
-          .then((savedDocument) => {
-            logger.log({
-              level: "info",
-              email: req.body.email,
-              message: "Success. Account created.",
-            });
-            return res.status(200).json({
-              status: "200",
-              message: "Success. Account created.",
-              email: req.body.email,
-              userID: savedDocument.userID,
-            });
-          })
-          .catch((err) => {
-            logger.log({
-              level: "error",
-              email: req.body.email,
-              message: "Not able to save the instance.",
-            });
-            return res
-              .status(400)
-              .json({ code: "400", message: "Error while signing up." });
-          });
-      }
-    })
-    .catch((err) => {
-      logger.log({
-        level: "error",
-        email: req.body.email,
-        message: "Lookup for email failed.",
-      });
-      return res
-        .status(400)
-        .json({ code: "400", message: "Error while signing up." });
-    });
+
+  //Display error if input format is wrong
+  await validationError(errors, email, res);
+  //Signup Service to add user details to DB
+  signupService(filter, email, password, res);
 };
 
 //Signin controller
-const signIn = (req, res) => {
+const signIn = async (req, res) => {
   //ValidationResult is from middleware used in signup route
   const errors = validationResult(req);
-  //logger.log(errors); For debugging only
-  if (!errors.isEmpty()) {
-    logger.log({
-      level: "error",
-      email: req.body.email,
-      message: "Invalid email address.",
-    });
-    return res.status(400).json({
-      status: "400",
-      message: "Invalid email address.",
-    });
-  }
-  //Find the email address in DB
+  const email = req.body.email;
+  const password = req.body.password;
+  const sessionID = req.sessionID;
   const filter = { email: req.body.email };
-  dbFind(user, filter)
-    .then((document) => {
-      if (document.length === 0) {
-        logger.log({
-          level: "error",
-          email: req.body.email,
-          message: "Email address not found in database.",
-        });
-        return res
-          .status(400)
-          .json({
-            code: "400",
-            message: "Email address not found in database.",
-          });
-      } else {
-        sessionID = req.sessionID;
-        //Decode the encoded password from DB and check with input recieved.
-        if (req.body.password === base64.decode(document[0].password)) {
-          const filter = { email: req.body.email };
-          const update = { sessionID: sessionID };
-          dbUpdate(user, filter, update)
-            .then((document) => {
-              logger.log({
-                level: "info",
-                email: req.body.email,
-                message: "Welcome! Successfully signed in.",
-              });
-              return res.status(200).json({
-                status: "200",
-                message: "Welcome! Successfully signed in.",
-                email: req.body.email,
-                sessionID: sessionID,
-              });
-            })
-            .catch((err) => {
-              logger.log({
-                level: "error",
-                email: req.body.email,
-                message: "Updation of session id failed.",
-              });
-              return res.status(400).json({
-                code: "400",
-                message: "Error while signing in.",
-              });
-            });
-        } else {
-          logger.log({
-            level: "error",
-            email: req.body.email,
-            message: "Wrong Password.",
-          });
-          return res
-            .status(400)
-            .json({ code: "400", message: "Passwords don't match." });
-        }
-      }
-    })
-    .catch((err) => {
-      logger.log({
-        level: "error",
-        email: req.body.email,
-        message: "Signin find() causing error",
-      });
-      return res
-        .status(400)
-        .json({ code: "400", message: "Error while signing in." });
-    });
+  const update = { sessionID: req.sessionID };
+
+  //Display error if input format is wrong
+  await validationError(errors, email, res);
+  //Signin service to verify user credentials in DB
+  signinService(filter, update, email, password, sessionID, res);
 };
 
 //Signout controller
-const signOut = (req, res) => {
+const signOut = async (req, res) => {
   //ValidationResult is from middleware used in signup route
   const errors = validationResult(req);
-  //logger.log(errors); For debugging only
-  if (!errors.isEmpty()) {
-    logger.log({
-      level: "error",
-      email: req.body.email,
-      message: "Error: Invalid email address.",
-    });
-    return res.status(400).json({
-      status: "400",
-      message: "Error: Invalid email address.",
-    });
-  }
-  if (req.session) {
-    //console.log(req.sessionID); For debugging only
-    //Delete sessionID in request
-    delete req.sessionID;
-    //Find and delete sessionID in DB
-    const filter = { email: req.body.email };
-    const update = { sessionID: "None" };
-    dbUpdate(user, filter, update)
-      .then((document) => {
-        if (document === null) {
-          logger.log({
-            level: "error",
-            email: req.body.email,
-            message: "Deletion Error: No document to update. Empty document returned.",
-          });
-          return res
-            .status(400)
-            .json({ code: "400", message: "Error: Unable to signout." });
-        } else {
-          //console.log(req.sessionID); For debugging only
-          logger.log({
-            level: "info",
-            email: req.body.email,
-            message: "Successfully signed out.",
-          });
-          //Clear cookie in response
-          return res.clearCookie("connect.sid").status(200).json({
-            status: "200",
-            message: "Successfully signed out.",
-            email: req.body.email,
-          });
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-        logger.log({
-          level: "error",
-          email: req.body.email,
-          message: "Update() error: Deletion of session ID failed.",
-        });
-        return res
-          .status(400)
-          .json({ code: "400", message: "Error: Unable to signout." });
-      });
-  }
+  const email = req.body.email;
+
+  //Display error if input format is wrong
+  await validationError(errors, email, res);
+  //Service to will delete the sessionID and signout the user
+  signoutService(req, res);
 };
 
 module.exports = { signUp, signIn, signOut };
---
+++ src/services/userServices.js
@@ -0,0 +1,216 @@
+//File for services that do certain tasks when called from user controller
+
+const base64 = require("base-64");
+const user = require("../models/userModel");
+const { logger } = require("../logger");
+const { dbSave, dbFind, dbUpdate } = require("../db/dbOperations");
+
+//Display error when input format is wrong
+const validationError = (errors, email, res) => {
+  //logger.log(errors); For debugging only
+  if (!errors.isEmpty()) {
+    logger.log({
+      level: "error",
+      email: email,
+      message: "Error: Invalid input format.",
+    });
+    return res.status(400).json({
+      status: "400",
+      message: "Error: Invalid input format.",
+    });
+  }
+};
+
+//Signup Service to add user details to DB
+const signupService = (filter, email, password, res) => {
+  if (res.statusCode !== 400) {
+    //Check if the user is already signed up.
+    dbFind(user, filter)
+      .then((document) => {
+        if (document.length !== 0) {
+          logger.log({
+            level: "error",
+            email: email,
+            message: "Email address already in use.",
+          });
+          return res
+            .status(400)
+            .json({ code: "400", message: "Email address already in use." });
+        } else {
+          //Encoding the password in base64 format
+          const encodedPassword = base64.encode(password);
+          //Create the document
+          const document = new user({
+            email: email,
+            password: encodedPassword,
+          });
+          //Save the user credentials to DB
+          dbSave(document)
+            .then((savedDocument) => {
+              logger.log({
+                level: "info",
+                email: email,
+                message: "Success. Account created.",
+              });
+              return res.status(200).json({
+                status: "200",
+                message: "Success. Account created.",
+                email: email,
+                userID: savedDocument.userID,
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              logger.log({
+                level: "error",
+                email: email,
+                message: "Not able to save the instance.",
+              });
+              return res
+                .status(400)
+                .json({ code: "400", message: "Error while signing up." });
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        logger.log({
+          level: "error",
+          email: email,
+          message: "Lookup for email failed.",
+        });
+        return res
+          .status(400)
+          .json({ code: "400", message: "Error while signing up." });
+      });
+  }
+};
+
+//Signin service to verify user credentials in DB
+const signinService = (filter, update, email, password, sessionID, res) => {
+  if (res.statusCode !== 400) {
+    //Check if the user credentials are in DB
+    dbFind(user, filter)
+      .then((document) => {
+        if (document.length === 0) {
+          logger.log({
+            level: "error",
+            email: email,
+            message: "Email address not found in database.",
+          });
+          return res.status(400).json({
+            code: "400",
+            message: "Email address not found in database.",
+          });
+        } else {
+          //Decode the encoded password from DB and check with input recieved.
+          if (password === base64.decode(document[0].password)) {
+            //Update the generated sessionID in DB
+            dbUpdate(user, filter, update)
+              .then((document) => {
+                logger.log({
+                  level: "info",
+                  email: email,
+                  message: "Welcome! Successfully signed in.",
+                });
+                return res.status(200).json({
+                  status: "200",
+                  message: "Welcome! Successfully signed in.",
+                  email: email,
+                  sessionID: sessionID,
+                });
+              })
+              .catch((err) => {
+                logger.log({
+                  level: "error",
+                  email: email,
+                  message: "Updation of session id failed.",
+                });
+                return res.status(400).json({
+                  code: "400",
+                  message: "Error while signing in.",
+                });
+              });
+          } else {
+            logger.log({
+              level: "error",
+              email: email,
+              message: "Wrong Password.",
+            });
+            return res
+              .status(400)
+              .json({ code: "400", message: "Wrong Password." });
+          }
+        }
+      })
+      .catch((err) => {
+        logger.log({
+          level: "error",
+          email: email,
+          message: "Signin find() causing error",
+        });
+        return res
+          .status(400)
+          .json({ code: "400", message: "Error while signing in." });
+      });
+  }
+};
+
+//Service to will delete the sessionID and signout the user
+const signoutService = (req, res) => {
+  if (res.statusCode !== 400) {
+    if (req.session) {
+      //console.log(req.sessionID); For debugging only
+      //Delete sessionID in request
+      delete req.sessionID;
+      const filter = { email: req.body.email };
+      const update = { sessionID: "None" };
+      //Remove the sessionID in DB
+      dbUpdate(user, filter, update)
+        .then((document) => {
+          if (document === null) {
+            logger.log({
+              level: "error",
+              email: req.body.email,
+              message:
+                "Deletion Error: No document to update. Empty document returned.",
+            });
+            return res
+              .status(400)
+              .json({ code: "400", message: "Error: Unable to signout." });
+          } else {
+            //console.log(req.sessionID); For debugging only
+            logger.log({
+              level: "info",
+              email: req.body.email,
+              message: "Successfully signed out.",
+            });
+            //Clear cookie in response
+            return res.clearCookie("connect.sid").status(200).json({
+              status: "200",
+              message: "Successfully signed out.",
+              email: req.body.email,
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          logger.log({
+            level: "error",
+            email: req.body.email,
+            message: "Update() error: Deletion of session ID failed.",
+          });
+          return res
+            .status(400)
+            .json({ code: "400", message: "Error: Unable to signout." });
+        });
+    }
+  }
+};
+
+module.exports = {
+  validationError,
+  signupService,
+  signinService,
+  signoutService,
+};
